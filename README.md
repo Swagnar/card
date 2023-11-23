049c7d174a4d6bf5fd994fb32a8d3e34dc92f770
Swagnar <derkarus@gmail.com>
2023-11-23 22:24:35 +0100

    finally drop lists, work dialog window, changed tux.png - reverted commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # card
 
-This project is supposed to be my show-off <br>
+Hello and welcome. Learn about my work, hobbies and more through this application<br>
 
 Note: heavily inspired by [Kanye2049](https://kanye2049.com/)
 
--- script.js
+++ script.js
@@ -1,63 +1,51 @@
+const dialogContents = {
+  about: "<p>Made by <a href='https://github.com/Swagnar'>Swagnar</a></p><p>Inspired by <a href='https://kanye2049.com'>Kanye2049</a></p>"
+}
+
+
+
+
 window.onload = function() {
-  var toggledDrop;
-  
+    const dialog = document.getElementById('dialog')
+    const dialogBody = document.getElementById('dialog-body')
+
     const navbarItems = Array.from(document.querySelectorAll('.item > button'))
-
-
     const navbarParentElements = navbarItems.map((item) => item.parentElement)
     const navbarDropLists = navbarItems.map((item) => item.nextElementSibling)
-
-    console.log(navbarParentElements)
-    console.log(navbarDropLists)
-
     
-
     navbarItems.forEach((item, i) => {
-      
       item.addEventListener('click', () => {
-        navbarDropLists.forEach((list) => {
-          list.style.opacity = "0"
-          list.style.visibility = "hidden"
-        })
+
+        const isActive = navbarParentElements[i].classList.contains('active');
+    
         navbarParentElements.forEach((item) => {
-          item.classList.remove('active')
-        })
-        if(!navbarParentElements[i].classList.contains('active')) {
-          navbarParentElements[i].classList.add('active')
-          navbarDropLists[i].style.opacity = "1"
-          navbarDropLists[i].style.visibility = "visible"
-        } else {
-          navbarParentElements[i].classList.remove('active')
-          navbarDropLists[i].style.opacity = "0"
-          navbarDropLists[i].style.visibility = "hidden"
+          item.classList.remove('active');
+        });
+        navbarDropLists.forEach((list) => {
+          list.style.opacity = '0';
+          list.style.visibility = 'hidden';
+        });
+    
+        if (!isActive) {
+          console.log(navbarParentElements[i].classList);
+    
+          navbarParentElements[i].classList.add('active');
+          navbarDropLists[i].style.opacity = '1';
+          navbarDropLists[i].style.visibility = 'visible';
         }
-      })
-      
+      });
+    });
+
+    document.getElementById('dialog-close-btn').addEventListener('click', () => {
+      dialog.classList.remove('dialog-open')
+      dialog.classList.add('dialog-hidden')
     })
 
-    
-
-    // // tux logo
-    // const logoParentElement = logo.parentElement
-    
-    // // ul .drop list
-    // const logoNextElementSibling = logo.nextElementSibling
-    
-    // if(toggledDrop === undefined) {
-    //   toggledDrop = logoNextElementSibling
-    // }
-
-    // logo.addEventListener('click', () => {
-    //   if(!logoParentElement.classList.contains('active')) {
-    //     logoParentElement.classList.toggle('active')
-    //     logoNextElementSibling.style.opacity = "1"
-    //     logoNextElementSibling.style.visibility = "visible"
-    //   } else {
-    //     logoParentElement.classList.toggle('active')
-    //     logoNextElementSibling.style.opacity = "0"
-    //     logoNextElementSibling.style.visibility = "hidden"
-    //   }
-    // })
-  
-
+    document.getElementById('about').addEventListener('click', () => {
+      if(!dialog.classList.contains('dialog-open')) {
+        dialog.classList.remove('dialog-hidden')
+        dialog.classList.add('dialog-open')
+      }
+      dialogBody.innerHTML = dialogContents.about
+    }) 
 }
