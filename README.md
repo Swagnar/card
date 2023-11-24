2e4d59cfa0f8ee7889a314746cbc56bff0763574
Swagnar <derkarus@gmail.com>
2023-11-24 10:24:06 +0100

    added desktop flickering animation, right side of the navbar, updated README

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 Hello and welcome. Learn about my work, hobbies and more through this application<br>
 
-Note: heavily inspired by [Kanye2049](https://kanye2049.com/)
+## Notes 
+- heavily inspired by [Kanye2049](https://kanye2049.com/)
+
+- flickering animation made by [Alec Lowens](aleclownes.com)
+
+
 
 
--- script.js
+++ script.js
@@ -6,46 +6,71 @@ const dialogContents = {
 
 
 window.onload = function() {
-    const dialog = document.getElementById('dialog')
-    const dialogBody = document.getElementById('dialog-body')
+  const dialog = document.getElementById('dialog')
+  const dialogBody = document.getElementById('dialog-body')
 
-    const navbarItems = Array.from(document.querySelectorAll('.item > button'))
-    const navbarParentElements = navbarItems.map((item) => item.parentElement)
-    const navbarDropLists = navbarItems.map((item) => item.nextElementSibling)
-    
-    navbarItems.forEach((item, i) => {
-      item.addEventListener('click', () => {
+  const datetime = document.getElementById('datetime')
 
-        const isActive = navbarParentElements[i].classList.contains('active');
-    
-        navbarParentElements.forEach((item) => {
-          item.classList.remove('active');
-        });
-        navbarDropLists.forEach((list) => {
-          list.style.opacity = '0';
-          list.style.visibility = 'hidden';
-        });
-    
-        if (!isActive) {
-          console.log(navbarParentElements[i].classList);
-    
-          navbarParentElements[i].classList.add('active');
-          navbarDropLists[i].style.opacity = '1';
-          navbarDropLists[i].style.visibility = 'visible';
-        }
+  const navbarItems = Array.from(document.querySelectorAll('.item > button'))
+  const navbarParentElements = navbarItems.map((item) => item.parentElement)
+  const navbarDropLists = navbarItems.map((item) => item.nextElementSibling)
+  
+  navbarItems.forEach((item, i) => {
+    item.addEventListener('click', () => {
+
+      const isActive = navbarParentElements[i].classList.contains('active');
+  
+      navbarParentElements.forEach((item) => {
+        item.classList.remove('active');
       });
-    });
-
-    document.getElementById('dialog-close-btn').addEventListener('click', () => {
-      dialog.classList.remove('dialog-open')
-      dialog.classList.add('dialog-hidden')
-    })
-
-    document.getElementById('about').addEventListener('click', () => {
-      if(!dialog.classList.contains('dialog-open')) {
-        dialog.classList.remove('dialog-hidden')
-        dialog.classList.add('dialog-open')
+      navbarDropLists.forEach((list) => {
+        list.style.opacity = '0';
+        list.style.visibility = 'hidden';
+      });
+  
+      if (!isActive) {
+        console.log(navbarParentElements[i].classList);
+  
+        navbarParentElements[i].classList.add('active');
+        navbarDropLists[i].style.opacity = '1';
+        navbarDropLists[i].style.visibility = 'visible';
       }
-      dialogBody.innerHTML = dialogContents.about
-    }) 
+    });
+  });
+
+  document.getElementById('dialog-close-btn').addEventListener('click', () => {
+    dialog.classList.remove('dialog-open')
+    dialog.classList.add('dialog-hidden')
+  })
+
+  document.getElementById('about').addEventListener('click', () => {
+    if(!dialog.classList.contains('dialog-open')) {
+      dialog.classList.remove('dialog-hidden')
+      dialog.classList.add('dialog-open')
+    }
+    dialogBody.innerHTML = dialogContents.about
+  }) 
+
+
+
+
+
+  function update_clock() {
+    var now = new Date()
+    var month = now.toLocaleString('en-us', {month: 'long'})
+    
+    var hours = now.getHours()
+    var minutes = now.getMinutes()
+
+    hours = hours < 10 ? '0' + hours : hours
+    minutes = minutes < 10 ? '0' + minutes : minutes
+    
+    datetime.innerText = `${hours}:${minutes} - ${month.slice(0,3)}. ${now.getDay()}, ${now.getFullYear()}`
+  
+    
+  }
+
+  setInterval(update_clock, 1000)
+
+  update_clock()
 }
